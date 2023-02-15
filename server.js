a76c8f911ad3dfc121410b87738b766fda522f69
saivenkat523 <20h51a0523@cmrcet.ac.in>
2023-02-15 21:57:26 +0530

    adding few changes

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+require ("dotenv").config();
+const mongoose =require("mongoose");
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5010;
@@ -7,8 +9,19 @@ app.get("/", function(req, res){
 	//res.send("Iam siavenkat");
 	res.sendFile(__dirname+"/resume.html");
 });
-
-app.listen(port, function(){
-	console.log("Server running on http://localhost:"+port);
-	console.log(`Server running on http://localhost:${port}`);
+mongoose.set('strictQuery', true);
+mongoose.connect(process.env.MONGO_CONNECTION_STRING,{},function(err){
+     if(err)
+	 {
+		console.error(err);
+	 }
+	 else
+	 {
+		console.log("DB connected");
+		app.listen(port, function(){
+			console.log("Server running on http://localhost:"+port);
+			console.log(`Server running on http://localhost:${port}`);
+		});
+	 }
 });
+
