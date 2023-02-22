e5fceac8245c19863ebf84bd4fdf131671415697
saivenkat523 <20h51a0523@cmrcet.ac.in>
2023-02-22 19:48:03 +0530

    commited

--- server.js
+++ server.js
@@ -1,87 +1,88 @@
 require("dotenv").config();
 const userLib = require("./backend/lib/userLib");
-const TodoLib= require("./backend/lib/todoLib");
+const todoLib = require("./backend/lib/todoLib");
 const mongoose = require("mongoose");
 const express = require('express');
+const { request } = require("express");
 const app = express();
 const port = process.env.PORT || 5010;
 const options = {
 	extensions:['htm','html','css','js','ico','jpg','jpeg','png','svg','pdf'],
 	index:['index.html'],
 }
-app.use(express.static("public",options));
+
+app.use(express.static("frontend"));
 app.use(express.json());
-app.get("/card", function(req, res){
-	res.sendFile(__dirname+"/card.html");
-});
-app.get("api/todo",function(req,res)
-{
-	TodoLib.getAllTodos(function(err,todos){
-		if(err)
-		{
-			res.json({status:"error",message:err,data:null});
+app.use(express.static("public",options));
+
+app.get("/api/todos",function(req,res){
+	todoLib.getAllTodos(function(err, todos){
+		if(err){
+			res.json({status: "error", message: err, data: null});
 		}
-		else
-		{
-			res.json({staus:"success",data:todos});
+		else{
+			res.json({status: "success", data: todos});
 		}
 	});
 });
-app.post("api/todo",function(req,res)
-{
-	const todo=req.body;
-	TodoLib.createTodos(function(err,todos){
-		if(err)
-		{
-			res.json({status:"error",message:err,data:null});
+
+app.post("/api/todos", function(req, res){
+	const todo = req.body;
+	todoLib.createTodo(todo, function(err, dbtodo){
+		if(err){
+			res.json({status: "error", message: err, data: null});
 		}
-		else
-		{
-			res.json({staus:"success",data:todos});
+		else{
+			res.json({status: "success", data: dbtodo});
 		}
 	});
 });
-app.put("/api/todo/:todoid",function(req,res)
-{
-	const todo=req.body;
-	const todoid=req.params.todoid;
-	TodoLib.updateTodoByid(todoid,todo,function(err,todo)
-	{
-		if(err)
-		{
-			res.json({status:"error",message:err,data:null});
+
+app.put(("/api/todos/:todoid"),function(req,res){
+	const todo = req.body;
+	const todoid = req.params.todoid;
+	todoLib.updateTodoById(todoid, todo, function(err, dbtodo){
+		if(err){
+			res.json({status: "error", message: err, data: null});
 		}
-		else
-		{
-			res.json({staus:"success",data:todos});
+		else{
+			res.json({status: "success", data: dbtodo});
 		}
 	});
 });
-app.delete("/api/todo/:todoid",function(req,res)
-{
-    const todoid=req.params.todoid;
-	TodoLib.deleteTodoById(todoid,function(err,todo){
-		if(err)
-		{
-			res.json({status:"error",message:err,data:null});
+
+app.delete(("/api/todos/:todoid"),function(req,res){
+	const todoid = req.params.todoid;
+	todoLib.deleteTodoById(todoid, function(err,dbtodo){
+		if(err){
+			res.json({status: "error", message: err, data: null});
 		}
-		else
-		{
-			res.json({staus:"success",data:todos});
+		else{
+			res.json({status: "success", data: dbtodo});
 		}
 	});
 });
 
 app.get("/resume", function(req, res){
-	res.sendFile(__dirname+"/resume.html");
+	res.sendFile(__dirname+"/resume");
 });
 
 app.get("/1", function(req, res){
-	res.sendFile(__dirname+"/1.html");
+	res.sendFile(__dirname+"/frontend/html/1.html");
+});
+
+app.get("/card", function(req, res){
+	res.sendFile(__dirname+"/card.html");
 });
 app.get("/weather", function(req, res){
 	res.sendFile(__dirname+"/weather.html");
 });
+// app.get("/todolist", function(req, res){
+// 	res.sendFile(__dirname+"/frontend/html/todolist.html");
+// });
+// app.get("/about", function(req, res){
+// 	res.sendFile(__dirname+"/frontend/html/about.html");
+// });
 mongoose.set('strictQuery', true);
 mongoose.connect(process.env.MONGO_CONNECTION_STRING,{},function (err){
 	if(err){
@@ -89,6 +90,40 @@ mongoose.connect(process.env.MONGO_CONNECTION_STRING,{},function (err){
 	}
 	else{
 		console.log("DB Connected");
+
+
+
+		// todoLib.createTodo({title: "nithin vp"}, function(err,res){
+		// 	if(err){
+		// 		console.error(err);
+		// 	}
+		// 	else{
+		// 		console.log(res);
+		// 	}
+		// });
+		// todoLib.getAllTodos(function(err,res){
+		// 	if(err){
+		// 		console.error(err);
+		// 	}
+		// 	else{
+		// 		console.log(res);
+		// 	}
+		// });
+		// todoLib.getSingleTodoById({title: "nithin vp"}, function(err,res){
+		// 	if(err) console.error(err);
+		// 	else console.log(res);
+		// });
+
+		// todoLib.getTodosByQuery({isCompleted: false, isDeleted: false}, function(err,res){
+		// 	if(err){
+		// 		console.error(err);
+		// 	}
+		// 	else{
+		// 		console.log(res);
+		// 	}
+		// });
+		// todoLib.updateTodoById()
+
 		// TODO: donot create user if atleast 1 user exist int the table
 		// userLib.createFirstUser(function(err,res){
 		// 	if(err){
@@ -114,15 +149,15 @@ mongoose.connect(process.env.MONGO_CONNECTION_STRING,{},function (err){
 		// 		console.log(result);
 		// 	}
 		// });
-		userLib.deleteUser("saivenkat",function(err,result){
-			if(err){
-				console.error(err);
-			}
-			else{
-				console.log(result);
-			}
-		});
-		// userLib.getUserByFilter({userName: "saivenkat"}, function(err,result){
+		// userLib.deleteUser("Srikanth Reddy",function(err,result){
+		// 	if(err){
+		// 		console.error(err);
+		// 	}
+		// 	else{
+		// 		console.log(result);
+		// 	}
+		// });
+		// userLib.getUserByFilter({userName: "Srikanth Reddy"}, function(err,result){
 		// 	if(err){
 		// 		console.error(err);
 		// 	}
